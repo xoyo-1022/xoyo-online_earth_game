ddb8f8c134f994690c2aba04af18b3bac4d97cd1
xoyo-1022 <xueyu131420@126.com>
2026-05-26 14:50:50 +0800

    修复 Next.js 配置

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/xoyo-online_earth_game',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
